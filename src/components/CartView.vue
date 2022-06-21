b69b79e81b3c620d31553cb181ca77cb5e475ca7
Emilie Décosterd <emilie.decosterd@gmail.com>
2022-06-21 13:41:57 +0200

    CartView

--- src/components/CartView.vue
+++ src/components/CartView.vue
@@ -1,3 +1,9 @@
 <template>
+  <!-- Si il y a quelque chose dans le panier (CartView) -> Affiche le nombre d'items dans le rond orange du cart
+      Sinon n'affiche rien
 
+      Créer un array vide par défaut qui est rempli quand le user clique sur Add to cart
+      Le CartView reprend le contenu de l'array
+      Le CartView ne s'affiche que si click sur cart
+  -->
 </template>
